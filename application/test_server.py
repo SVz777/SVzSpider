ef2d7949f02260d52acaba13340522c805e173f1
SVz <liqizhao@didiglobal.com>
2019-03-20 19:53:51 +0800

    test server

---
+++ application/test_server.py
@@ -0,0 +1,16 @@
+import flask
+
+app = flask.Flask('app')
+
+app.route('/<int:a>')
+
+
+def test(a):
+    return f'''
+    <a href='{a - 1}'>prev</a>
+    <p>{a}</p>
+    <a href='{a + 1}'>next</a>
+    '''
+
+
+app.run('127.0.0.1', '8888')
